02ecbf592ef5fdf22901108cdd5c10122a4fc29b
Fofiko <foteinid@gmail>
2018-10-29 22:10:08 +0000

    Add FilmReleases container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import FilmReleases from './containers/FilmReleases'
 
 class App extends Component {
   render() {
     return (
-      <div className="film-releases">
-        
-      </div>
+      <FilmReleases />
     );
   }
 }
---
+++ src/containers/FilmReleases.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class FilmReleases extends Component {
+    render() {
+        return (
+            <div className="film-releases">Upcoming film releases here</div>
+        )
+    }
+}
+
+export default FilmReleases
